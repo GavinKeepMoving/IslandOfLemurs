aa7c4d42579283ced96905ec282fab9e07474bb2
Huang Zhenni <suzu@Suzu-3.local>
2014-09-27 13:53:19 -0700

    added fsm

---
+++ brave/Classes/FSM.cpp
@@ -0,0 +1,9 @@
+//
+//  FSM.cpp
+//  brave
+//
+//  Created by Huang Zhenni on 9/27/14.
+//
+//
+
+#include <stdio.h>
---
+++ brave/Classes/FSM.h
@@ -0,0 +1,13 @@
+//
+//  FSM.h
+//  brave
+//
+//  Created by Huang Zhenni on 9/27/14.
+//
+//
+
+#ifndef brave_FSM_h
+#define brave_FSM_h
+
+
+#endif
