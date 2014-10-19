1ac7e780e3835c678f7c8de969567757ca515f07
Wenbo Lin <lwbshr@gmail.com>
2014-10-19 10:36:08 -0700

    fix monkey drop down bug

--- brave/Classes/MainScene.cpp
+++ brave/Classes/MainScene.cpp
@@ -581,19 +581,21 @@ void MainScene::deleteTree() {
     float rangeLeft = 0;
     float rangeRight = 0;
     
+    /*
     if(_trees.size() - 1 == 0) {
         rangeLeft = xPos - target->getContentSize().width / 2;
         rangeRight = xPos + target->getContentSize().width / 2;
         this->boundry = target->_posX - target->getContentSize().width*5/4 - visibleSize.width/2;
         _player->playerDrop(rangeLeft, rangeRight);
     }
-    else {
+     */
+//    else {
         rangeLeft = xPos - target->getContentSize().width - rope->getContentSize().width * 0.5;
         rangeRight = xPos + target->getContentSize().width / 2;
         this->boundry = target->_posX - target->getContentSize().width*5/4 - rope->getContentSize().width - visibleSize.width/2;
         std::function<void()> onWalk = CC_CALLBACK_0(Player::onWalk, _player, this->touchPos, this->boundry);
         _player->playerDrop(rangeLeft, rangeRight, onWalk);
-    }
+//    }
     
     _trees.pop_back();
     
